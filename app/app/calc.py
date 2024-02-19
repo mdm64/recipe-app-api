a739364c54e3d690e2df81232f5ac1465c28f2be
mayank <2018eeb1164@iitrpr.ac.in>
2024-02-19 07:57:19 +0530

    123

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+# calculator function
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+# sample tests
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_number(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11 )
+
+    def test_sub_numbers(self):
+        res = calc.sub(5,4)
+
+        self.assertEqual(res, 1)
